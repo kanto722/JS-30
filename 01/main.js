6fa0e88bf4bcd1aa42516a86ece504cd4b779672
Kanto <kanto72@yandex.ru>
2022-04-08 00:36:12 +0300

    fix fail

---
+++ 01/main.js
@@ -0,0 +1,24 @@
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.toggle('playing');
+  }
+
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  
+  this.classList.remove('playing');
+  console.log(this);
+
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
+window.addEventListener('keyup', removeTransition);
+
+
